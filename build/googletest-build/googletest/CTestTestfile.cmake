9feeb159b9c40de9a13892f1bd1098853d9cdf57
Brandon Smith <brandon.kev.smith@gmail.com>
2020-05-31 14:08:33 -0600

    clean up

--- build/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/USU/2019-2020/CS3460/TypeAhead/build/googletest-src/googletest
-# Build directory: E:/USU/2019-2020/CS3460/TypeAhead/build/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
